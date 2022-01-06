89ea5b6bf46ea475f779e73703d327406604a956
bushart hussain <busharthussain@gmail.com>
2022-01-06 19:01:03 +0500

    change namespaces

--- kiswatech/kiswapackage/src/TestPackageController.php
+++ kiswatech/kiswapackage/src/TestPackageController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace bash\bashpackage;
+namespace kiswatech\kiswapackage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- kiswatech/kiswapackage/src/TestPackageServiceProvider.php
+++ kiswatech/kiswapackage/src/TestPackageServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace bash\bashpackage;
+namespace kiswatech\kiswapackage;
 
 use Illuminate\Support\ServiceProvider;
 
